4244c8f59a2ee4abc6c3d6a48659d28f14e53bfc
komaragiri1980 <137947770+komaragiri1980@users.noreply.github.com>
2023-07-13 22:38:43 -0500

    fix path

--- src/main/java/baseclass/Testbase.java
+++ src/main/java/baseclass/Testbase.java
@@ -3,6 +3,7 @@ package baseclass;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
@@ -119,7 +120,7 @@ public class Testbase {
 	  File source = ts.getScreenshotAs(OutputType.FILE);
 	  
 	  // after execution, you could see a folder "FailedTestsScreenshots" under source folder
-	  String destination = System.getProperty("user.dir") + "/Screenshots/" + screenshotName + dateName + ".png";
+	  String destination = Paths.get("").toAbsolutePath() + "/Screenshots/" + screenshotName + dateName + ".png";
 	  File finalDestination = new File(destination);
 	  FileUtils.copyFile(source, finalDestination);
 	  return destination;
